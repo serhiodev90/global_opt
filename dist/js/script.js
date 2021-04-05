de9e8adafcf3b74005106bf1e9dad6972c4f4b7b
serhiodev90 <lokom286@yandex.ru>
2021-04-05 22:33:04 +0300

    First Advantages_Block (HTML)

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,6 +3,6 @@ $(document).ready(function(){
 	//Burger
 
 	$('.header__burger').click(function(event){
-		$('.header__burger, .header__menu').toggleClass('active');
+		$('.header__burger, .header__menu, .contact').toggleClass('active');
 	});
 });
