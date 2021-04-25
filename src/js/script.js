87553cb35df01330b1766454429c770c06c8f477
serhiodev90 <lokom286@yandex.ru>
2021-04-25 03:59:56 +0300

    First Reviews_Block

--- src/js/script.js
+++ src/js/script.js
@@ -5,4 +5,33 @@ $(document).ready(function(){
 	$('.header__burger').click(function(event){
 		$('.header__burger, .header__menu, .contact').toggleClass('active');
 	});
+
+	//Slids
+
+	$('.slids').slick({
+		infinite: true,
+		centerMode: true,
+		centerPadding: '60px',
+		slidesToShow: 3,
+		responsive: [
+			{
+			 breakpoint: 768,
+			 settings: {
+				arrows: false,
+				centerMode: true,
+				centerPadding: '40px',
+				slidesToShow: 3
+				}
+		  	},
+		  	{
+			 breakpoint: 480,
+			 settings: {
+				arrows: false,
+				centerMode: true,
+				centerPadding: '40px',
+				slidesToShow: 1
+			 	}
+		  	}
+		]
+	});
 });
